7a671bf49a4b447f6c4bdfec7d825c548e9e542d
Hellen Cristina Nascimento porta <hellen.nascii@hotmail.com>
2022-04-27 17:42:15 -0300

    projeto

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'bdpet';
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+if ($conexao-> connect_errno)
+{
+    echo "Erro";
+}
+else
+{
+    echo "Conexao efetuada com sucesso";
+}
+?>
